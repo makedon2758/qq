8365b45200826380d9ad47f067614c5478ad0e7b
makedon2758 <121632492+makedon2758@users.noreply.github.com>
2024-02-27 00:22:03 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,18 +1,5 @@
 (() => {
     "use strict";
-    function isWebp() {
-        function testWebP(callback) {
-            let webP = new Image;
-            webP.onload = webP.onerror = function() {
-                callback(webP.height == 2);
-            };
-            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-        }
-        testWebP((function(support) {
-            let className = support === true ? "webp" : "no-webp";
-            document.documentElement.classList.add(className);
-        }));
-    }
     let bodyLockStatus = true;
     let bodyLockToggle = (delay = 500) => {
         if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
@@ -69,6 +56,5 @@
         }
     }), 0);
     window["FLS"] = true;
-    isWebp();
     menuInit();
 })();
